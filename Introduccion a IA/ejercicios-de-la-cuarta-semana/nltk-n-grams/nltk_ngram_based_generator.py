1af3887ee0c081826d7a30dd8590be540daeb673
jaimeberriosm <jaimeberriosm@gmail.com>
2024-10-25 18:56:36 -0300

    modified:   Introduccion a IA/ejercicios-de-la-cuarta-semana/nltk-n-grams/nltk_ngram_based_generator.py

--- Introduccion a IA/ejercicios-de-la-cuarta-semana/nltk-n-grams/nltk_ngram_based_generator.py
+++ Introduccion a IA/ejercicios-de-la-cuarta-semana/nltk-n-grams/nltk_ngram_based_generator.py
@@ -8,7 +8,7 @@ text = open('Introduccion a IA/ejercicios-de-la-cuarta-semana/nltk-n-grams/poe.t
 tokenized_text = [list(map(str.lower, word_tokenize(sent))) 
                   for sent in sent_tokenize(text)]
 
-n = 3
+n = 5
 train_data, padded_sents = padded_everygram_pipeline(n, tokenized_text)
 model = MLE(n)
 model.fit(train_data, padded_sents)
